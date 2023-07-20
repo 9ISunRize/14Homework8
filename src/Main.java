4cfc3ed30e8913e8e5c2bbb03a131cdf7a986623
Диана <dianamingazova162@gmail.com>
2023-07-21 01:05:05 +0300

    Обновила работу

--- src/Main.java
+++ src/Main.java
@@ -20,63 +20,67 @@ public class Main {
     public static void task2() {
         System.out.println("Задача 2");
         int[] array1 = {1, 2, 3};
-        int nomber1 = array1[0];
-        array1[1] = 2;
-        int nomber2 = array1[1];
-        array1[2] = 3;
-        int nomber3 = array1[2];
-        System.out.print(array1[0] + ",");
-        System.out.print(array1[1] + ",");
-        System.out.println(array1[2] + " ");
+        for (int i = 0; i < array1.length; i++) {
+            if (i == array1.length - 1) {
+                System.out.print(array1[i]);
+            } else {
+                System.out.print(array1[i] + ",");
+            }
+        }
+        System.out.println(" ");
         double array2[] = new double[]{1.57, 7.654, 9.986};
-        for (int i = 0; i < 2; i++) {
-            System.out.print(array1[i] + ",");
+        for (int i = 0; i < array2.length; i++) {
+            if (i == array1.length - 1) {
+                System.out.print(array2[i]);
+            } else {
+                System.out.print(array2[i] + ",");
+            }
         }
-        System.out.println(array2[2] + " ");
-
-
+        System.out.println(" ");
         int[] myArray = new int[]{16, 21, 25,};
-        for (int i = 0; i < 2; i++) {
-            System.out.print(myArray[i] + ",");
+        for (int i = 0; i < myArray.length; i++) {
+            if (i == array1.length - 1) {
+                System.out.print(myArray[i]);
+            } else {
+                System.out.print(myArray[i] + ",");
+            }
         }
-        System.out.println(myArray[2] + " ");
+        System.out.println(" ");
     }
-
     public static void task3() {
         System.out.println("Задача 3");
         int[] array1 = {1, 2, 3};
-        int nomber1 = array1[0];
-        array1[1] = 2;
-        int nomber2 = array1[1];
-        array1[2] = 3;
-        int nomber3 = array1[2];
-        System.out.print(array1[2] + ",");
-        System.out.print(array1[1] + ",");
-        System.out.println(array1[0] + " ");
-
+        for (int i = 2; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(array1[i]);
+            } else {
+                System.out.print(array1[i] + ",");
+            }
+        }
+        System.out.println(" ");
         double[] array2 = new double[]{1.57, 7.654, 9.986};
-        for (int i = 2; i >= 1; i--) {
-            System.out.print(array2[i] + ",");
+        for (int i = 2; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(array2[i]);
+            } else {
+                System.out.print(array2[i] + ",");
+            }
         }
-        System.out.println(array2[0] + " ");
-
+        System.out.println(" ");
         int[] myArray = new int[]{16, 21, 25,};
-        for (int i = 2; i >= 1; i--) {
-            System.out.print(myArray[i] + ",");
+        for (int i = 2; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(myArray[i]);
+            } else {
+                System.out.print(myArray[i] + ",");
+            }
         }
-        System.out.println(myArray[0] + " ");
-
         System.out.println(" ");
     }
 
     public static void task4() {
         System.out.println("Задача 4");
         int[] array1 = {1, 2, 3};
-        int nomber1 = array1[0];
-        array1[1] = 2;
-        int nomber2 = array1[1];
-        array1[2] = 3;
-        int nomber3 = array1[2];
         for (int i = 0; i < 3; i++) {
             if (array1[i] % 2 != 0) {
                 array1[i] = array1[i] + 1;
