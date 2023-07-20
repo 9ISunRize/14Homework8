3393c07c6c7b5e52562d5eef48663138d9362287
Диана <dianamingazova162@gmail.com>
2023-07-21 01:09:38 +0300

    Обновила работу

--- src/Main.java
+++ src/Main.java
@@ -82,7 +82,7 @@ public class Main {
     public static void task4() {
         System.out.println("Задача 4");
         int[] array1 = {1, 2, 3};
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < array1.length; i++) {
             if (array1[i] % 2 != 0) {
                 array1[i] = array1[i] + 1;
             }
